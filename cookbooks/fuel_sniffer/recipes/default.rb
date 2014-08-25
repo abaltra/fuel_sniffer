f641f02d83d75af456bbe858e681aedb8e775b7d
Alejandro Baltra <abaltra@gmail.com>
2014-08-25 09:46:05 -0400

    Updated recipes

--- cookbooks/fuel_sniffer/recipes/default.rb
+++ cookbooks/fuel_sniffer/recipes/default.rb
@@ -8,6 +8,11 @@
 #
 include_recipe 'apt'
 
+execute 'curl -sL https://deb.nodesource.com/setup | sudo bash -' do
+  command 'curl -sL https://deb.nodesource.com/setup | sudo bash -'
+  action :run
+end
+
 package 'tesseract-ocr' do
   action :install
 end
@@ -24,7 +29,21 @@ package 'python-pip' do
   action :install
 end
 
-execute 'pip install beautifulsuop4' do
+package 'git' do
+  action :install
+end
+
+execute 'pip install beautifulsoup4' do
   command 'pip install beautifulsoup4'
   action :run
+end
+
+execute 'npm install -g eskimo' do
+  command 'npm install -g eskimo' do
+  action :run
+end
+
+execute 'install eskimo dependencies' do
+  command 'npm install'
+  cwd '/fuel_sniffer/fuel_api'
 end
